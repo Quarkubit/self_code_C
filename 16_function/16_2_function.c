7fc725df000148f7a837741568119583c42a0d25
Quarkubit <AbramovYD+github@gmail.com>
2023-09-25 15:10:47 +0300

    lesson 6
    completly finished

--- 16_function/16_2_function.c
+++ 16_function/16_2_function.c
@@ -1,17 +1,43 @@
 #include "fun.h"
 
 void sum(int a, int b);
+void string(char arr[]);
+float mult(float a, float b);
+
+
 
 int main(){
     sum(20,50);
+
     int x=5,y=7;
     sum(x,y);
 
+    string("hello");
 
+    float result = mult(4.5f, 7.8f);
+    printf("%.2f\n",result);
+
+
+
+    system("pause");
     return 0;
 }
+
+
+
+
 
 void sum(int a, int b) {
     int res = a+b;
     printf("result: %d\n",res);
+}
+
+void string(char arr[]) {
+    printf("%s\n",arr);
+}
+
+float mult(float a, float b){
+    float res = a * b;
+    printf("result: %.2f\n",res);
+    return res;
 }
