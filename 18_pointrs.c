cb989fe210a2d5d2076f1385c51b5c72363b08e9
Quarkubit <AbramovYD+github@gmail.com>
2023-09-26 12:54:33 +0300

    unfinished 8 lesson

--- 18_pointrs.c
+++ 18_pointrs.c
@@ -3,10 +3,17 @@
 
 int main() {
 
-    int num = 0;
+    int Num = 2;
     //scanf("%d",&num);     &num - это и есть указатель
 
-    printf("%p\n", &num);//указатель имеет лучшую опимизацию
+    int *pNum = &Num;//переменная-указатель 
+
+    printf("%p - %p\n", &Num, pNum);//указатель имеет лучшую опимизацию
+
+    printf("%d\n", *pNum);
+    *pNum=91;
+    printf("%d\n", *pNum);
+
 
 
 
